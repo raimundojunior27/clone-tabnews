46e71c51e42ec2d99f387fc417a3439d8be918bf
Rjr21 <84860507+raimundojunior27@users.noreply.github.com>
2025-01-26 21:02:47 +0000

    modifica frase para a versao da vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h2>Teste do git para diff e fazer o amend ao repositorio </h2>
+    return <h2>Teste do git para diff e fazer o amend ao repositorio fazer o deploy para vercel </h2>
+    
 }
 
 export default Home;
