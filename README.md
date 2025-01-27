7845af798832c5431b36b0b50b5a83a81b9cc995
Rjr21 <84860507+raimundojunior27@users.noreply.github.com>
2025-01-27 00:07:51 +0000

    adiciona o script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Curso 
+
+Curso
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 function Home() {
-    return <h2>Teste do git para diff e fazer o amend ao repositorio fazer o deploy para vercel VERSAO 02 </h2>
-    
+  return (
+    <h2>
+      Teste do git para diff e fazer o amend ao repositorio fazer o deploy para
+      vercel VERSAO 02{" "}
+    </h2>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
