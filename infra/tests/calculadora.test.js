5aeb0abfd5987444b5e89d89232635dff6bb474d
Rjr21 <84860507+raimundojunior27@users.noreply.github.com>
2025-02-03 23:19:54 +0000

    criando o teste inicial e vendo como funciona o jest

---
+++ infra/tests/calculadora.test.js
@@ -0,0 +1,16 @@
+const calculadora = require("../../models/calculadora.js");
+
+test("somar 2 + 2 retorna 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100 retorna 105", () => {
+  const resultado = calculadora.somar(100, 5);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 retorna 'Erroo'", () => {
+  const resultado = calculadora.somar("banana", 5);
+  expect(resultado).toBe("Erro");
+});
